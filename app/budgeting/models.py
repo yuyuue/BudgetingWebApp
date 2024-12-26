a5f2a94c30fef8d029919c2107a47ae94bca3141
YuYu <uuprivate224m@gmail.com>
2024-12-26 10:21:51 +0900

    [update] Add fields to asset-related classes
    Added fields to the 'AssetCategory', 'Asset' and 'Credit' classes.

--- app/budgeting/models.py
+++ app/budgeting/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Asset(models.Model):
-    pass
-
-
 class AssetCategory(models.Model):
     pass
 
 
+class Asset(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(AssetCategory, on_delete=models.PROTECT)
+    amount = models.IntegerField
+
+
 class Credit(models.Model):
-    pass
-
-
-class Income(models.Model):
     pass
 
 
@@ -21,9 +19,13 @@ class IncomeCategory(models.Model):
     pass
 
 
-class Expence(models.Model):
+class Income(models.Model):
     pass
 
 
 class ExpenceCategory(models.Model):
+    pass
+
+
+class Expence(models.Model):
     pass
