139d25da4c6aa6f0659e79bc912e04eb458d3255
YuYu <uuprivate224m@gmail.com>
2024-12-26 09:18:20 +0900

    [update] Create model classes
    Created the model classes required for this app in `app/budgeting/models.py`.

--- app/budgeting/models.py
+++ app/budgeting/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+    pass
+
+
+class AssetCategory(models.Model):
+    pass
+
+
+class Credit(models.Model):
+    pass
+
+
+class Income(models.Model):
+    pass
+
+
+class IncomeCategory(models.Model):
+    pass
+
+
+class Expence(models.Model):
+    pass
+
+
+class ExpenceCategory(models.Model):
+    pass
