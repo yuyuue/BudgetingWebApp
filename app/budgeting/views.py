1f47a5a132f45176aa3e5d21d270a1ba20b6f2bc
YuYu <uuprivate224m@gmail.com>
2024-12-26 15:43:15 +0900

    [add] Initialize templates and views
    Added initial templates and defined classes in `views.py` to prepare for view creation.

--- app/budgeting/views.py
+++ app/budgeting/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+class AssetListView():
+    pass
+
+
+class AssetDetailView():
+    pass
+
+
+class AssetCategoryListView():
+    pass
+
+
+class AssetCategoryDetailView():
+    pass
