91b7d833dafdc8c13840947c6c9c29ee880ae490
YuYu <uuprivate224m@gmail.com>
2024-12-28 16:52:25 +0900

    [add] Add forms to edit asset
    Added two forms for editing asset details

--- app/budgeting/views.py
+++ app/budgeting/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views import generic
 
 from .models import Asset, AssetCategory
@@ -13,6 +15,7 @@ class AssetListView(generic.ListView):
 class AssetDetailView(generic.DetailView):
     model = Asset
     template_name = 'asset/asset_detail.html'
+    context_object_name = 'asset'
 
 
 class AssetCategoryListView(generic.ListView):
@@ -24,3 +27,14 @@ class AssetCategoryListView(generic.ListView):
 class AssetCategoryDetailView(generic.DetailView):
     model = AssetCategory
     template_name = 'asset/category_detail.html'
+    context_object_name = 'asset_category'
+
+
+def mod_asset(request, asset_id):
+    asset = get_object_or_404(Asset, pk=asset_id)
+    return HttpResponseRedirect(reverse('budgeting:asset_detail', args=(asset.id,)))
+
+
+def mod_asset_category(request, asset_category_id):
+    asset_category = get_object_or_404(Asset, pk=asset_category_id)
+    return HttpResponseRedirect(reverse('budgeting:asset_category', args=(asset_category.id,)))
