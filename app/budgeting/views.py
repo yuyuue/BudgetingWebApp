ece6b3143574ff2c524b407497abf0ddfdfbaa9e
YuYu <uuprivate224m@gmail.com>
2024-12-29 16:54:30 +0900

    [add] Initialize cash flow edit page
    Added templates for cash flow list and editing page.
    
    Created models for managing cash flow.
    
    Updated `admin.py` and `urls.py` to register and access the models.

--- app/budgeting/views.py
+++ app/budgeting/views.py
@@ -3,9 +3,35 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from .models import Asset, AssetCategory
+from .models import Asset, AssetCategory, CashFlow, CashFlowCategory
 
 # Create your views here.
+def mod_asset(request, asset_id):
+    asset = get_object_or_404(Asset, pk=asset_id)
+
+    # Modify asset fields.
+    asset.name = request.POST['name']
+    asset.amount = request.POST['amount']
+    # Info relate to credit.
+    if 'asset_is_credit' in request.POST:
+        asset.withdrawal_account = request.POST['withdrawal_account']
+        asset.payment_due_date = request.POST['payment_due_date']
+        asset.payment_confirmation_date = request.POST['payment_confirmation_date']
+    asset.save()
+    
+    return HttpResponseRedirect(reverse('budgeting:asset_detail', args=(asset.id,)))
+
+
+def mod_asset_category(request, asset_category_id):
+    asset_category = get_object_or_404(Asset, pk=asset_category_id)
+
+    # Modify asset category fields.
+    asset_category.name = request.POST['name']
+    asset_category.save()
+
+    return HttpResponseRedirect(reverse('budgeting:asset_category', args=(asset_category.id,)))
+
+
 class AssetListView(generic.ListView):
     model = Asset
     template_name = 'asset/asset_list.html'
@@ -29,28 +55,25 @@ class AssetCategoryDetailView(generic.DetailView):
     template_name = 'asset/category_detail.html'
     context_object_name = 'asset_category'
 
-
-def mod_asset(request, asset_id):
-    asset = get_object_or_404(Asset, pk=asset_id)
-
-    # Modify asset fields.
-    asset.name = request.POST['name']
-    asset.amount = request.POST['amount']
-    # Info relate to credit.
-    if 'asset_is_credit' in request.POST:
-        asset.withdrawal_account = request.POST['withdrawal_account']
-        asset.payment_due_date = request.POST['payment_due_date']
-        asset.payment_confirmation_date = request.POST['payment_confirmation_date']
-    asset.save()
-    
-    return HttpResponseRedirect(reverse('budgeting:asset_detail', args=(asset.id,)))
+class CashFlowListView(generic.ListView):
+    model = CashFlow
+    template_name = 'budgeting/cash_flow_list.html'
+    context_object_name = 'cash_flow_list'
 
 
-def mod_asset_category(request, asset_category_id):
-    asset_category = get_object_or_404(Asset, pk=asset_category_id)
+class CashFlowDetaiView(generic.DetailView):
+    model = CashFlow
+    template_name = 'budgeting/cash_flow_detail.html'
+    context_object_name = 'cash_flow'
 
-    # Modify asset category fields.
-    asset_category.name = request.POST['name']
-    asset_category.save()
-    
-    return HttpResponseRedirect(reverse('budgeting:asset_category', args=(asset_category.id,)))
+
+class CashFlowCategoryListView(generic.ListView):
+    model = CashFlowCategory
+    template_name = 'budgeting/category_list.html'
+    context_object_name = 'cash_flow_category_list'
+
+
+class CashFlowCategoryDetailView(generic.DetailView):
+    model = CashFlowCategory
+    template_name = 'budgeting/category_detail.html'
+    context_object_name = 'cash_flow_category'
